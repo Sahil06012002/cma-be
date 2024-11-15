f9cec0bd19912e03992a1fedb8113b4612c1f241
Sahil Ahmad <sahmad@EX2INDIA.LOCAL>
2024-11-15 12:00:07 +0530

    final commit

--- main.py
+++ main.py
@@ -1,15 +1,38 @@
-{
-  "version": 2,
-  "builds": [
-    {
-      "src": "main.py",
-      "use": "@vercel/python"
-    }
-  ],
-  "routes": [
-    {
-      "src": "/(.*)",
-      "dest": "/main.py"
-    }
-  ]
-}
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from middleware import AuthMiddleware
+from routes import product_route, user_route
+
+app = FastAPI()
+app.add_middleware(AuthMiddleware)
+
+origins = [
+    "http://localhost:5173",
+    "https://cma-fe-psi.vercel.app/"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,          
+    allow_credentials=True,          
+    allow_methods=["*"],             
+    allow_headers=["*"],             
+)
+
+app.include_router(user_route.router,prefix="/user")
+app.include_router(product_route.router,prefix="/product")
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello, World!"}
+
